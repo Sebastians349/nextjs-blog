0602dc1e4d7e82fc935b2337b5ca8ea17a618c0b
Sebastian Simarro <ssebastian210@gmail.com>
2021-09-09 14:55:39 -0300

    <404><check>falta estilizar

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,16 @@
 // pages/404.js
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div>
+      <h1 className="centrado">404 - 🙈 La página no está</h1>;
+      <h2>¿Por qué se llamara 404?</h2>;
+      <p>
+        In the past, it was speculated that error 404 was named after a room at
+        CERN (European Organization of Nuclear Research) where the original web
+        servers were located. However, this speculation was disproved by Tom S.,
+        who said that he couldn’t find Room 404 when he visited.
+        {/* FIX TRADUCIR  */}
+      </p>
+    </div>
+  );
 }
